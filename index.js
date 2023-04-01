4919c354dbe385e041b2711cac8c551f6f865b0c
abdel <ammegahe@amazon.com>
2023-04-01 09:31:58 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function(driver) {
+      return driver.toLowerCase() === name.toLowerCase();
+    });
+  }
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+      return driver.name === name;
+    });
+  }
+
+
